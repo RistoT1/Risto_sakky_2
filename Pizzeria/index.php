faecb3f02fd82b8cd400aa8dfc9a6b4574f59c31
Risto Toivanen <ristotoiv.rt@gmail.com>
2025-08-28 15:07:31 +0300

    Add files via upload

---
+++ Pizzeria/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <main>
+        <div class="container">
+            <div class="header-container">
+                <div class="Header">
+                    <div class="title">
+                        <h1>Sakky Pizzeria</h1>
+                    </div>
+                    <div class="header-links">
+                        <a href="#">Home</a>
+                        <a href="#">Menu</a>
+                        <a href="#">Contact</a>
+                    </div>
+                </div>
+            </div>
+            <div class="content">
+                <h2>Tervetuloa Sakky Pizzeriaan!</h2>
+                <p>Sakky opiskelijoille suunniteltu pizzeria.</p>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
